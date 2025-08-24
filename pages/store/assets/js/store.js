6aeb546729bc027e3d30148c4e6be25efd4ec179
lutherdev <devturd@gmail.com>
2025-08-24 20:23:54 +0800

    updated fuggcategory

--- pages/store/assets/js/store.js
+++ pages/store/assets/js/store.js
@@ -24,31 +24,3 @@ fetch('/FUGGIES/staticData/json/fugg.json')
 })
 .catch(err => console.error('Error loading products:', err));
 
-
-//Fart fugg - P750 (damaged box P700)
-
-// New Fuggs on the block- P550
-
-// Festive fugg - P400 each/Pair P750
-
-// Gold Fuggs - P600
-
-// Family Fuggs - P700
-
-// Gremlins - P900 each/ P1600 Pair
-
-// Mini Rascals - P900
-
-// LOTR - P700each /P1200Pair / P1700 Set of 3
-
-// Glow - P600
-
-// DC - P750 each / P1300 Pair / P1800 Pair of 3 / P2300 Pair of 4
-
-// Love Fugg - P900
-
-// Keyrings P450 (Chase me button P550)
-
-// Baby fugg P450
-// Algernon
-// Hairy  fuggs - P650
