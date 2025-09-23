698928639da2ec4203af240829a808305acb111e
lutherdev <devturd@gmail.com>
2025-09-23 20:33:26 +0800

    feat: added about us page

---
+++ pages/about-us/assets/js/store.js
@@ -0,0 +1,35 @@
+fetch('/staticData/json/fugg.json')
+.then(res => res.json())
+.then(products => {
+    products.forEach(product => {
+        // find the right category container
+        const categoryDiv = document.querySelector(
+            `.fuggCategory[name="${product.category}"]`
+        );
+
+        if (!categoryDiv){
+            const categoryDiv = document.querySelector(
+            `.fuggCategoryKey[name="${product.category}"]`
+        );
+        }
+
+        if (categoryDiv) {
+            // create the box
+            const box = document.createElement('div');
+            box.classList.add('fuggBox');
+            //box.style.backgroundColor = product.color;
+            box.innerHTML = `
+                <div class="img"><img src="${product.img}" alt="${product.name}"></div>
+                <div class="desc">${product.name} | ₱${product.price} | ${product.quantity} left</div>
+            `;
+            categoryDiv.appendChild(box);
+        } else {
+            console.warn(`No category container found for: ${product.category}`);
+        }
+    });
+})
+.catch(err => console.error('Error loading products:', err));
+
+
+
+
